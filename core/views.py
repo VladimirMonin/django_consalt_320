f181e4a809e86d003c56121af9b64a62c261e64a
v <v@mail.ru>
2024-11-02 13:30:07 +0500

    fix: добавил недостающий шаблон спасибо и формы

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from django.shortcuts import render, redirect
-from .forms import VisitModelForm, VisitEditModelForm
+from .forms import VisitModelForm
 from .models import Visit, Master, Service
 from django.http import JsonResponse
 from django.views.generic import (
