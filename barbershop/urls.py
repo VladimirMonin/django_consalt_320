8ebf4c117d70fa156a09742ae4e7c0f41e3be2a8
v <v@mail.ru>
2024-11-02 12:57:23 +0500

    urls: настройка машрутов проекта

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -19,8 +19,22 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
+from core.views import (
+    ServicesByMasterView,
+    MainView,
+    ThanksTemplateView,
+
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", MainView.as_view(), name="main"),
+    path("thanks/", ThanksTemplateView.as_view(), name="thanks"),
+    path(
+        "get_services_by_master/<int:master_id>/",
+        ServicesByMasterView.as_view(),
+        name="get_services_by_master",
+    ),
 ]
 
 
