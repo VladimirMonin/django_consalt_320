6f883197b0a1d7153d397b5f1afd58f83c40ae45
v <v@mail.ru>
2024-11-16 14:41:11 +0500

    установили django-jazzmin и улучшили внешний вид админки

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',  # Должен быть первым!
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
